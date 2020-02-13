b5085c9d8938cb59f1853defb83056c39010e322
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-02-13 14:44:30 -0500

    added map to landing page and styled

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import HomeCarousel from "../components/HomeCarousel";
-import { Button } from "../shared/Button";
+import About from '../components/About'
+import { Image } from '../shared/Image'
+import { Button } from "../shared/Button"
+import MapContainer from '../components/Map'
 const storePic = require("../images/store-pic.jpg");
-
+const storePic2 = require("../images/pic-slide-3.jpg")
 class Landing extends React.Component {
   constructor() {
     super();
@@ -12,32 +15,27 @@ class Landing extends React.Component {
     return (
       <>
         <HomeCarousel />
-        <img src={storePic} alt="store" className="store-pic" />
-        <div className='about'>
-          <h2>ABOUT FUSION WIRELESS</h2>
-          <p>
-            We care at Fusion Wireless. After all, we've been keeping businesses
-            and families connected for over 25 years! Sure there are many places
-            to get a cell phone, but are they giving you the best price possible
-            with the care and attention you deserve?
-          </p>
-          <p> 
-            At Fusion, our dedicated
-            and experienced staff will ensure you are getting the phone that
-            best suits your interests, not ours. Our communications specialists
-            are there for you for the life of your phone; you will always have
-            our direct email address. And in the unlikely event your unhappy
-            with your purchase, bring it back. If you're not completely
-            satisfied, neither are we. 
-            </p>
-            <p>Let us know how we can better serve you.
-            Give us a call today at 877-933-CELL and see what we can do for you!
-          </p>
-          <Button
-            title='Need Help? 877-933-CELL'
-            className='need-help-button'
-          />
-        </div>
+        <Image
+          src={storePic}
+          alt="store"
+          className="store-pic" />
+        <About />
+        <Image
+          src={storePic2}
+          alt="store2"
+          className="animated-pics" />
+        <Button
+          title='CAREERS'
+          className='animatedPics-button' />
+        <Image
+          src={storePic}
+          alt="store"
+          className="animated-pics" />
+        <Button
+          title='CORPORATE SALES'
+          className="animatedPics-button" />
+        <MapContainer/>
+        
       </>
     );
   }
